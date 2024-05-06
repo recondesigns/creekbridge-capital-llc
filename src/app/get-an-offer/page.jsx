9ae20e767e1384f6601d19d3dbccf545980f22df
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-05 21:16:04 -0500

    added routes for pages

---
+++ src/app/get-an-offer/page.jsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+
+export default function GetAnOfferPage() {
+  return (
+    <div>Get An Offer</div>
+  )
+}
---
+++ src/app/terms-and-conditions/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TermsAndConditionsPage() {
+  return (
+    <div>Terms and Conditions Page</div>
+  )
+}
